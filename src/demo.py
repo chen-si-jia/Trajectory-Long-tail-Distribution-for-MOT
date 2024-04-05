89c21d1f201d8fe03d2705ad02629567c9e10fc4
chen-si-jia <58644677+chen-si-jia@users.noreply.github.com>
2024-04-05 16:20:50 +0800

    Update demo.py

--- src/demo.py
+++ src/demo.py
@@ -24,15 +24,17 @@ def demo(opt):
     logger.info('Starting tracking...')
     dataloader = datasets.LoadVideo(opt.input_video, opt.img_size)
     result_filename = os.path.join(result_root, 'results.txt')
-    frame_rate = dataloader.frame_rate
+
+    seq="MOT16-03"
 
     frame_dir = None if opt.output_format == 'text' else osp.join(result_root, 'frame')
+
     eval_seq(opt, dataloader, 'mot', result_filename,
-             save_dir=frame_dir, show_image=False, frame_rate=frame_rate,
+             seq=seq, save_dir=frame_dir,
              use_cuda=opt.gpus!=[-1])
 
     if opt.output_format == 'video':
-        output_video_path = osp.join(result_root, 'MOT16-03-results.mp4')
+        output_video_path = osp.join(result_root, seq+'-results.mp4')
         cmd_str = 'ffmpeg -f image2 -i {}/%05d.jpg -b 5000k -c:v mpeg4 {}'.format(osp.join(result_root, 'frame'), output_video_path)
         os.system(cmd_str)
 
